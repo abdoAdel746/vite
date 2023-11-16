1886c0bf211123aa8b81f4baaf210958485a207b
abdelrahman adel <abdelrahman20191700364@cis.asu.edu.eg>
2023-11-16 20:47:22 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Props } from "./components/props/Props";
+import {
+  SimpleGrid,
+  Heading,
+  Text,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Flex,
+  Box,
+  Spacer,
+  Image,
+  Container,
+  extendTheme,
+  withDefaultColorScheme,
+  background,
+} from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    {
+      id: 1,
+      img_src: "../src/assets/w.png",
+      name: "man hoodie",
+      describtion: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eumearum commodi`,
+      size: "s",
+      price: "500",
+      type: "white",
+    },
+    {
+      id: 2,
+      img_src: "../src/assets/d.png",
+      name: "man jaecket",
+      describtion: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eumearum commodi`,
+      size: "m",
+      price: "200",
+      type: "black",
+    },
+    {
+      id: 3,
+      img_src: "../src/assets/w.png",
+      name: "white hoodie",
+      describtion: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Eumearum commodi`,
+      size: "l",
+      price: "100",
+      type: "white",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {console.log(products)}
+      <Props products={products} />
+
+      {/* <Button_cart /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
